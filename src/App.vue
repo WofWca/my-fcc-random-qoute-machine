2308d7f275c1b7d8ea3fc23a6c21959fc2fd202c
WofWca <wofwca@gmail.com>
2019-03-14 21:45:31 +0800

    Add quote rotation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,49 @@
 <template>
-  <div id="app">
-    
+  <div id="app"
+    :style="{ backgroundColor: backgroundColor}"
+  >
+    <div id="quote-box">
+      <span id="text">{{currQuote.text}}</span>
+      <span id="author">{{currQuote.author}}</span>
+      <button id="new-quote"
+        @click="changeQuote"
+      >New Quote</button>
+      <a href="" id="tweet-quote"></a>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+interface Quote {
+  text: string;
+  author: string;
+}
+
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  private quotes: Quote[] = [
+    { text: 'With great power comes great responsibility', author: 'Uncle Ben? idk' },
+    { text: 'text 2', author: 'author 2' },
+    { text: 'text 3', author: 'author 3' },
+    { text: 'text 4', author: 'author 4' },
+  ];
+  private backgroundColor = '#555'; // TODO.
+  private currQuote = this.quotes[0];
+  private changeQuote(): void {
+    let nextQuoteInd: number = Math.floor(Math.random() * this.quotes.length);
+    if (nextQuoteInd === this.quotes.indexOf(this.currQuote)) {
+      const lastQuoteInd = this.quotes.length - 1;
+      if (nextQuoteInd === lastQuoteInd) {
+        nextQuoteInd = 0;
+      } else {
+        nextQuoteInd++;
+      }
+    }
+    this.currQuote = this.quotes[nextQuoteInd];
+  }
+}
 </script>
 
 <style lang="scss">
